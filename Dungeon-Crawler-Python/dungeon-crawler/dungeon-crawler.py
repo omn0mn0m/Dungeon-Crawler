dd31de069246c64f67cffbdc02d271b1321b19ee
omn0mn0m <tranngocnam97@yahoo.com>
2017-02-16 12:10:32 -0500

    Added ability to go to next room

--- Dungeon-Crawler-Python/dungeon-crawler/dungeon-crawler.py
+++ Dungeon-Crawler-Python/dungeon-crawler/dungeon-crawler.py
@@ -1,7 +1,19 @@
+import sys
+
 user_input = raw_input("You wake up in a dungeon. What do?")
 
-if user_input == "go to the next room":
-    print "You go to the next room."
+dungeon_map = 10
+current = 0;
 
 while user_input != "quit":
+    if user_input == "go to the next room":
+        if (current < 10):
+            print "You go to the next room."
+            current += 1
+        elif (current == 10):
+            print "You have escaped the dungeon."
+            raise SystemExit(0)
+        else:
+            print "You cannot go that way..."
+
     user_input = raw_input(">> ")
